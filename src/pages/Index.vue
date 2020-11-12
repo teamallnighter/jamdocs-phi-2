b05b39f023774c21d892529c938b7ae450a89423
Chris <me@chrisconnelly.dev>
2020-11-12 16:56:11 -0700

    Update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,9 +4,7 @@
       <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
-        <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
-        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+        <Shortcut link="/creating-new-shop-items" text="Get Started" icon="play-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
@@ -22,7 +20,6 @@ query {
 </static-query>
 
 <script>
-import GitLink from '~/components/GitLink.vue'
 import Shortcut from '~/components/Shortcut.vue'
 
 export default {
@@ -32,14 +29,14 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
+      description: 'Studio Phi Tattoos Tech Docs'
     }
   },
   metaInfo() {
     return {
       title: this.description,
       meta: [
-        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
+        { key: 'description', name: 'description', content: 'Tech docs for studio phi' }
       ]
     }
   }
